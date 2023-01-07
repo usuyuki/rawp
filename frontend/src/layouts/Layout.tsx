fa52793ce4fe85424d9033c0bf583e2b88cefe8d
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-07 10:33:40 +0900

    feat フロントエンド整備

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,50 @@
+import Footer from '@/layouts/Footer';
+import Header from '@/layouts/Header';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import type { ReactNode } from 'react';
+type Props = {
+    children: ReactNode;
+    title?: string;
+    description?: string;
+};
+
+const Layout: NextPage<Props> = ({ children, title, description }) => {
+    const formedTitle = title ? `${title} | RAWP` : 'RAWP';
+    const formedDescription =
+        description ?? 'RAWPは重複のない複数回グループ分けを素早く導き出すサービスです';
+
+    return (
+        <>
+            <Head>
+                <meta httpEquiv="content-language" content="ja" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
+                <meta name="description" content={formedDescription} />
+                <title>{formedTitle}</title>
+                {/* ogp */}
+                <meta property="og:title" content={formedTitle} />
+                <meta property="og:description" content={formedDescription} />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://rawp.usuyuki.net/" />
+                <meta property="og:image" content="https://rawp.usuyuki.net/img/ogp.png" />
+                {/* <meta property="fb:app_id"      content="" /> */}
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@usuyuki26" />
+                <meta name="twitter:creator" content="@usuyuki26" />
+
+                <link
+                    rel="apple-touch-icon"
+                    type="image/png"
+                    href="/img/favicon/apple-touch-icon-180x180.png"
+                />
+                <link rel="icon" type="image/png" href="/img/favicon/icon-192x192.png" />
+            </Head>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+};
+export default Layout;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import RawpConcept from '@/components/uiGroup/RawpConcept';
+import Layout from '@/layouts/Layout';
+import type { NextPage } from 'next';
+const IndexPage: NextPage = () => {
+    return (
+        <Layout>
+            <RawpConcept />
+        </Layout>
+    );
+};
+
+export default IndexPage;
