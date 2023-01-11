89fd550b404c52a00001bcf1ec278205db36d647
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-11 14:51:51 +0900

    wip material-iconの動作チェック

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,11 +2,12 @@ import RawpConcept from '@/components/uiGroup/RawpConcept';
 import Layout from '@/layouts/Layout';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import DescribeH3 from '@/components/uiParts/Heading/DescribeH3';
 const IndexPage: NextPage = () => {
     return (
         <Layout>
             <RawpConcept />
-
+<DescribeH3 heading="ああああ" sub="いいい" icon="search"/>
             <Link href="description">
                 <p>アルゴリズムについて</p>
             </Link>
