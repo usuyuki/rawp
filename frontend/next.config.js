072cc2d20a147115fbeef06775ea2e47bff57a36
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-15 10:28:54 +0900

    feat WASMを動かす環境整備

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    webpack: (config, { isServer }) => {
+        config.experiments = {
+            asyncWebAssembly: true,
+        };
+        config.output.webassemblyModuleFilename =
+            (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+        return config;
+    },
 };
 
 module.exports = nextConfig;
---
+++ script/cpWasm.sh
@@ -0,0 +1,2 @@
+cp -f kernel/pkg/rawpKernel_bg.wasm frontend/src/libs/rawpKernel_bg.wasm
+cp -f kernel/pkg/rawpKernel_bg.wasm.d.ts frontend/src/libs/rawpKernel_bg.wasm.d.ts
