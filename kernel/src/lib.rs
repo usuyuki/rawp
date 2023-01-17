aa7f331b37bf63f6f7523f29e68839069c54f4eb
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-17 21:55:44 +0900

    feat 重複度計算関数整備

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,19 +1,16 @@
 use wasm_bindgen::prelude::*;
-type MultipleTimesGroup = Vec<Vec<Vec<u8>>>; //分ける回数、グループ数、グループ人数 以後MTGと略す
+type MultipleTimesGroupVec = Vec<Vec<Vec<u8>>>; //分ける回数、グループ数、グループ人数 以後MTGと略す
+type duplicatinCountVec=Vec<Vec<u8>>;//人x人の配列
 
 // 参加者名を直接使う必要はないので、数字で管理してJS側で参加者名と紐づける
 // 必要な値は参加人数、グループ人数、分ける回数の3つで、u8とかで行ける(0~255)→JS側で255人以上は受け付けないようにする、実際200人超えは組み合わせの爆発の仕方的にも無理なので
 // returnで配列を返したいが、vec型returnはWASMにできないらしいので、文字列に変換して返し、JSで戻す
 #[wasm_bindgen]
-pub fn resolve_by_sa()->String{
-    //ここはあとで引数にする
-    let n_of_people:u8 = 12;
-    let n_of_group:u8 = 3;
-    let n_of_times:u8 = 3;
-
+pub fn resolve_by_sa(n_of_people:u8,n_of_group:u8,n_of_times:u8)->String{
     //焼きなまし法関連 基本イミュータブルだが、初期温度などはconstでなく、引数ベースで入るようにする
     let mut temp: f64 = 100.0;
 
+    //このへんf64にしてるけど、パラメータゲーなので意外と精度使うので意図的にf64。決してメモリの無駄遣いではない
     //初期温度
     let initial_temp: f64 = 100.0;
     //終了温度
@@ -22,30 +19,33 @@ pub fn resolve_by_sa()->String{
     let cooling_rate: f64 = 0.00009;
 
     //重複回数保存用の配列
-    let mut duplication: Vec<Vec<u8>> = vec![vec![0; n_of_people as usize]; n_of_people as usize];
+    let mut duplication:duplicatinCountVec  = vec![vec![0; n_of_people as usize]; n_of_people as usize];
 
 
     //グループ分けを格納する変数
-    let mut better_mtg:MultipleTimesGroup= vec![vec![vec![0; (n_of_people/n_of_group) as usize]; n_of_group as usize]; n_of_times as usize];
+    let mut better_mtg:MultipleTimesGroupVec= create_first_mtg(n_of_people, n_of_group, n_of_times);
     //グループ分けを格納する変数
-    let mut best_mtg:MultipleTimesGroup = vec![vec![vec![0; (n_of_people/n_of_group) as usize]; n_of_group as usize]; n_of_times as usize];
+    let mut best_mtg:MultipleTimesGroupVec =better_mtg;
+
+    //焼きなまし法のループ
+    while temp > end_temp{
 
 
-    //初期グループ分けを作成
-    for i in 0..n_of_times{
-        for j in 0..n_of_group{
-            for k in 0..(n_of_people/n_of_group){
-                better_mtg[i as usize][j as usize][k as usize] = (i*n_of_group*n_of_people/n_of_group + j*n_of_people/n_of_group + k) as u8;
-            }
-        }
+        temp*=cooling_rate;
     }
-    vec_to_string(best_mtg)
+
+
+
+
+
+    //文字列にしてお返ししてあげる
+    return vec_to_string(best_mtg);
 }
 
 //初期グループ分け生成
-fn create_first_mtg(n_of_people:u8, n_of_group:u8, n_of_times:u8)->MultipleTimesGroup{
+fn create_first_mtg(n_of_people:u8, n_of_group:u8, n_of_times:u8)->MultipleTimesGroupVec{
     //グループ分けを格納する変数
-    let mut mtg:MultipleTimesGroup= vec![vec![vec![0; (n_of_people/n_of_group) as usize]; n_of_group as usize]; n_of_times as usize];
+    let mut mtg:MultipleTimesGroupVec= vec![vec![vec![0; (n_of_people/n_of_group) as usize]; n_of_group as usize]; n_of_times as usize];
 
     let mut number:u8=0;
     //初期グループ分けを作成
@@ -58,12 +58,24 @@ fn create_first_mtg(n_of_people:u8, n_of_group:u8, n_of_times:u8)->MultipleTimes
         }
         number=0;
     }
-    mtg
+    return mtg;
 }
 
-//重複度の計算
-fn calcMultiplicity(){
+/**
+ * 重複度の計算 人数で割ることで人数の造花に依存しないようにしている
+ * counterはu32としてて、4294967295までに収まると信じている
+ * returnでf64もいらない気がするが、他もf64にしてるので贅沢にf64にする
+ */
+fn calc_multiplicity(duplication_array:duplicatinCountVec,n_of_people:u8)->f64{
+    let mut counter:u32=0;
 
+    for i in duplication_array{
+        for j in i{
+            counter+=j as u32;
+        }
+    }
+
+    return (counter as f64)/(n_of_people as f64);
 }
 
 /*
@@ -83,7 +95,7 @@ fn swap<T>(v: &mut Vec<Vec<T>>, i1: usize, j1: usize, i2: usize, j2: usize) {
 }
 
 //vec型配列から文字列に変える(WASMで配列returnできないので)
-fn vec_to_string(mut source_vec:MultipleTimesGroup)->String{
+fn vec_to_string(mut source_vec:MultipleTimesGroupVec)->String{
     let mut return_string:String=String::from("");
     for i in source_vec.iter_mut(){
         for j in i.iter_mut(){
@@ -92,7 +104,7 @@ fn vec_to_string(mut source_vec:MultipleTimesGroup)->String{
             }
         }
     }
-    return_string
+    return return_string;
 }
 
 
@@ -104,13 +116,13 @@ mod tests {
 
     #[test]
     fn test_create_first_mtg(){
-        let test_array:MultipleTimesGroup = create_first_mtg(12, 3, 4);
+        let test_array:MultipleTimesGroupVec = create_first_mtg(12, 3, 4);
         assert_eq!(vec![vec![vec![0, 1, 2, 3], vec![4, 5, 6, 7], vec![8, 9, 10, 11]], vec![vec![0, 1, 2, 3], vec![4, 5, 6, 7], vec![8, 9, 10, 11]], vec![vec![0, 1, 2, 3], vec![4, 5, 6, 7], vec![8, 9, 10, 11]], vec![vec![0, 1, 2, 3], vec![4, 5, 6, 7], vec![8, 9, 10, 11]]],test_array);
     }
 
     #[test]
     fn test_swap(){
-        let mut test_array:MultipleTimesGroup = vec![vec![vec![1,2],vec![3,4]],vec![vec![5,6],vec![7,8]],vec![vec![9,10],vec![11,12]]];
+        let mut test_array:MultipleTimesGroupVec = vec![vec![vec![1,2],vec![3,4]],vec![vec![5,6],vec![7,8]],vec![vec![9,10],vec![11,12]]];
         swap(&mut test_array[0],0,0,1,0);
         assert_eq!(vec![vec![vec![3,2],vec![1,4]],vec![vec![5,6],vec![7,8]],vec![vec![9,10],vec![11,12]]],test_array);
 
@@ -118,14 +130,21 @@ mod tests {
 
     #[test]
     fn test_vec_to_string(){
-        let test_array:MultipleTimesGroup = vec![vec![vec![1,2],vec![3,4]],vec![vec![5,6],vec![7,8]],vec![vec![9,10],vec![11,12]]];
+        let test_array:MultipleTimesGroupVec = vec![vec![vec![1,2],vec![3,4]],vec![vec![5,6],vec![7,8]],vec![vec![9,10],vec![11,12]]];
         assert_eq!("1,2,3,4,5,6,7,8,9,10,11,12,",vec_to_string(test_array));
     }
 
+    #[test]
+    fn test_calc_multiplicity(){
+        let test_array:duplicatinCountVec = vec![vec![1,4,0],vec![1,1,0],vec![1,1,1]];
+        assert_eq!(3.3333333333333335, calc_multiplicity(test_array,3));
+    }
 
-    // #[test]
-    // fn resolveBySA() {
-    //     let result = resolveBySA();
-    //     assert_eq!(result, 45);
-    // }
+
+    #[test]
+    fn resolveBySA() {
+        let result:String = resolve_by_sa(12,3,3);
+        print!("{:?}",result);
+        assert_eq!(12,result.chars().count());
+    }
 }
