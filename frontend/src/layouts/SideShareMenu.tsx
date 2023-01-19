6f81a9b190791ea0f3f8585c6b014bdb72f8fa16
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-19 13:01:10 +0900

    feat facebook share button 追加

--- frontend/src/layouts/SideShareMenu.tsx
+++ frontend/src/layouts/SideShareMenu.tsx
@@ -8,6 +8,7 @@ const SideShareMenu: NextPage = () => {
             <div className="flex flex-col items-center justify-center">
                 <p>share</p>
                 <div className="h-16 w-1 bg-black"></div>
+                {/* Twitter */}
                 <a
                     href="http://twitter.com/share?url=rawp.usuyuki.net&text=複数回のグループ分けをいい感じにするツール「RAWP」&related=@usuyuki26"
                     target="_blank"
@@ -16,6 +17,23 @@ const SideShareMenu: NextPage = () => {
                     <div className="flex h-12 w-12 items-center justify-center rounded-full bg-black">
                         <Image
                             src="/img/icon/TwitterLogoWhite.svg"
+                            alt="Twitter"
+                            width="0"
+                            height="0"
+                            sizes="100%"
+                            className="h-auto w-1/2"
+                        />
+                    </div>
+                </a>
+                {/* Facebook */}
+                <a
+                    href="https://www.facebook.com/share.php?u=http://rawp.usuyuki.net"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-black">
+                        <Image
+                            src="/img/icon/f_logo_RGB-White_250.png"
                             alt="Twitter"
                             width="0"
                             height="0"
