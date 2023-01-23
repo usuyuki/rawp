f2eb2a7af87807e269c3d57defff1fc2e8ba4cdc
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-01-23 13:40:59 +0900

    fix pnpm対応

--- script/frontBuild.sh
+++ script/frontBuild.sh
@@ -1,4 +1,4 @@
-docker-compose exec frontend yarn build
-docker-compose exec frontend yarn export
+docker-compose exec frontend pnpm build
+docker-compose exec frontend pnpm export
 # exportするとyarn devが動かなくなるので、再起動する
 docker-compose restart frontend
--- script/lint.sh
+++ script/lint.sh
@@ -1,2 +1,2 @@
-docker-compose exec frontend yarn lint:fix
+docker-compose exec frontend pnpm lint:fix
 docker-compose exec rust cargo fix --allow-no-vcs
