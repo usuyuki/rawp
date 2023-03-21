ab96e8c9cacd529922ece2ea89aeb996d26f1e5b
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-21 10:25:53 +0900

    fix 人のアイコンがずれている問題修正

--- frontend/src/components/uiGroup/DnD/Item.tsx
+++ frontend/src/components/uiGroup/DnD/Item.tsx
@@ -2,11 +2,13 @@ import { UniqueIdentifier } from "@dnd-kit/core";
 
 const Item = ({ id }: { id: UniqueIdentifier }) => {
   return (
-    <div className="m-2 rounded-xl border-2 p-2">
+    <div className="m-2 flex items-center rounded-xl border-2 p-2">
                                         <span className="material-symbols-outlined text-3xl">
                                             person
                                         </span>
+                                        <p>
       {id}
+                                        </p>
     </div>
   );
 };
